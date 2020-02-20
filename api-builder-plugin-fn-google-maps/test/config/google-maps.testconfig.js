d823006d1cea551d2d757827abbc36baa117e130
Chris Wiechmann <cwiechmann@axway.com>
2020-02-20 12:10:37 +0100

    Update google-maps.testconfig.js

--- api-builder-plugin-fn-google-maps/test/config/google-maps.testconfig.js
+++ api-builder-plugin-fn-google-maps/test/config/google-maps.testconfig.js
@@ -3,7 +3,7 @@ module.exports = {
 		'@axway-api-builder-ext/api-builder-plugin-fn-google-maps': {
 			'google': {
 				credentials: {
-					apiKey: 'AIzaSyDm7brbzgKNmGZIbe5gs0XDcep1zx5dv0s'
+					apiKey: process.env.GOOGLE_API_KEY
 				}
 			}
 		}
