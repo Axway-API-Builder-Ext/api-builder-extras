7d11364a2d7ddb8278cd8fc785dce46f57aa2630
Chris Wiechmann <cwiechmann@axway.com>
2020-02-20 15:36:29 +0100

    Added support for the Geocode API

--- api-builder-plugin-fn-google-maps/src/index.js
+++ api-builder-plugin-fn-google-maps/src/index.js
@@ -3,6 +3,7 @@ const { SDK } = require('@axway/api-builder-sdk');
 const { directions } = require('./directions')
 const { distance } = require('./distance')
 const { elevation } = require('./elevation')
+const { geocode } = require('./geocode')
 
 /**
  * Resolves the API Builder plugin.
@@ -10,12 +11,13 @@ const { elevation } = require('./elevation')
  */
 async function getPlugin(pluginConfig, options) {
 	const sdk = new SDK();
-	//sdk.load(path.resolve(__dirname, 'flow-nodes.yml'), directions);
-	sdk.load(path.resolve(__dirname, 'flow-nodes.yml'), { directions, distance, elevation });
+	debugger;
+	sdk.load(path.resolve(__dirname, 'flow-nodes.yml'), { directions, distance, elevation, geocode });
 	const plugin = sdk.getPlugin();
 	plugin.flownodes['googleMaps'].methods.directions.action = directions.bind({pluginConfig});
 	plugin.flownodes['googleMaps'].methods.distance.action = distance.bind({pluginConfig});
 	plugin.flownodes['googleMaps'].methods.elevation.action = elevation.bind({pluginConfig});
+	plugin.flownodes['googleMaps'].methods.geocode.action = geocode.bind({pluginConfig});
 	return sdk.getPlugin();
 }
 
---
+++ api-builder-plugin-fn-google-maps/test/geocoding-test.js
@@ -0,0 +1,91 @@
+const { expect } = require('chai');
+const { MockRuntime } = require('@axway/api-builder-sdk');
+
+const getPlugin = require('../src');
+const actions = require('../src/geocode');
+
+const validPluginConfig = require('./config/google-maps.testconfig').pluginConfig['@axway-api-builder-ext/api-builder-plugin-fn-google-maps'];
+const invalidPluginConfig = require('./config/google-maps.incomplete').pluginConfig['@axway-api-builder-ext/api-builder-plugin-fn-google-maps'];
+
+describe('Google-Maps Elevation-API Tests', () => {
+	let runtime;
+	before(async () => invalidRuntime = new MockRuntime(await getPlugin(invalidPluginConfig)));
+	before(async () => runtime = new MockRuntime(await getPlugin(validPluginConfig)));
+
+	describe('#constructor', () => {
+		it('should define flow-nodes', () => {
+			expect(actions).to.be.an('object');
+			expect(actions.elevation).to.be.a('function');
+			expect(runtime).to.exist;
+			const flownode = runtime.getFlowNode('googleMaps');
+			expect(flownode).to.be.a('object');
+			expect(flownode.name).to.equal('Google Maps');
+		});
+
+		// It is vital to ensure that the generated node flow-nodes are valid
+		// for use in API Builder. Your unit tests should always include this
+		// validation to avoid potential issues when API Builder loads your
+		// node.
+		it('should define valid flow-nodes', () => {
+			expect(runtime.validate()).to.not.throw;
+		});
+	});
+
+	describe('#geocode', () => {
+		it('should error when missing parameter: address', async () => {
+			const flowNode = runtime.getFlowNode('googleMaps');
+
+			const result = await flowNode.geocode({
+				address: null
+			});
+
+			expect(result.callCount).to.equal(1);
+			expect(result.output).to.equal('error');
+			expect(result.args[0]).to.equal(null);
+			expect(result.context).to.be.an('Object');
+			expect(result.context.error).to.have.property('message', 'Missing required parameter: address');
+		});
+
+		it('should error if not configured - API-Key is missing', async () => {
+			const flowNode = invalidRuntime.getFlowNode('googleMaps');
+
+			const result = await flowNode.geocode({
+				address: 'Premier Inn Frankfurt Messe, Europa-Allee, Frankfurt am Main'
+			});
+
+			expect(result.callCount).to.equal(1);
+			expect(result.output).to.equal('error');
+			expect(result.args[0]).to.equal(null);
+			expect(result.context).to.be.an('Object');
+			expect(result.context.error).to.have.property('message', 'Google API-Key is missing. Please complete your configuration in conf/google-maps.default.js');
+		});
+
+		it('Valid request with a location to geocode', async () => {
+			const flowNode = runtime.getFlowNode('googleMaps');
+
+			const result = await flowNode.geocode({
+				address: 'Premier Inn Frankfurt Messe, Europa-Allee, Frankfurt am Main'
+			});
+
+			expect(result.callCount).to.equal(1);
+			expect(result.output).to.equal('next');
+			expect(result.args[0]).to.equal(null);
+			expect(result.context).to.be.an('Object');
+			expect(result.context.result).to.deep.include({ status: 'OK' });
+		});
+
+		it.only('Request with an invalid location', async () => {
+			const flowNode = runtime.getFlowNode('googleMaps');
+
+			const result = await flowNode.geocode({
+				address: 'This location/address doesnt exists. XXXXXXXXXXX'
+			});
+
+			expect(result.callCount).to.equal(1);
+			expect(result.output).to.equal('notFound');
+			expect(result.args[0]).to.equal(null);
+			expect(result.context).to.be.an('Object');
+			expect(result.context.notFound).to.deep.include({ status: 'ZERO_RESULTS' });
+		});
+	});
+});
