59e0a4e510141bc4adb0df51edeba9040ca10cd7
Chris Wiechmann <cwiechmann@axway.com>
2020-04-13 18:02:15 +0200

    Trying to support both ways of testing: Mock & Integration

--- api-builder-plugin-fn-redis/test/test-config/basic-config.js
+++ api-builder-plugin-fn-redis/test/test-config/basic-config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	pluginConfig: {
 		'@axway-api-builder-ext/api-builder-plugin-fn-redis': {
+			// When host is set to 'MOCK' all call to Redis are mocked
+			//host: 'MOCK', 
 			host: 'api-env', 
 			port: '16379'
 		}
