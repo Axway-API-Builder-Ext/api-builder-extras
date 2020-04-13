a721ad326c238616c98747819faea729ab0f5782
Chris Wiechmann <cwiechmann@axway.com>
2020-04-13 18:06:12 +0200

    Using the MOCK configuration

--- api-builder-plugin-fn-redis/test/test-config/basic-config.js
+++ api-builder-plugin-fn-redis/test/test-config/basic-config.js
@@ -2,8 +2,8 @@ module.exports = {
 	pluginConfig: {
 		'@axway-api-builder-ext/api-builder-plugin-fn-redis': {
 			// When host is set to 'MOCK' all call to Redis are mocked
-			//host: 'MOCK', 
-			host: 'api-env', 
+			host: 'MOCK', 
+			//host: 'api-env', 
 			port: '16379'
 		}
 	}
