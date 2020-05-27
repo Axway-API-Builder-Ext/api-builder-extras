d66f715069c7c4e950786d05db4698981ca7cf5a
Plamen Todorov <ptodorov@axway.com>
2020-05-27 08:20:24 +0100

    clean up

--- api-builder-plugin-fn-redis/src/utils.js
+++ api-builder-plugin-fn-redis/src/utils.js
@@ -18,7 +18,7 @@ module.exports = {
     sdk.load(path.resolve(__dirname, 'flow-nodes.yml'), actions, { pluginContext });
     const plugin = sdk.getPlugin();
 
-    if (isDeveloperMode && process.env.TEST_TYPE === 'integration') {
+    if (process.env.TEST_TYPE === 'integration') {
       // Set ctx to plugin only in dev mode so we are able to test it
       plugin.redisClient = pluginContext.redisClient;
     }
@@ -48,27 +48,24 @@ module.exports = {
       instance.on(event, hooks[event]);
     });    
   },
-
-  isDeveloperMode
-}
-
-/**
- * Tests whether or not the API Builder application is in developer mode.  The test
- * is to check to see if @axway/api-builder-admin exists.
- *
- * @returns {boolean} True if in developer mode.
- */
-function isDeveloperMode() {
-  try {
-    // If we are in "development mode" we are going to have @axway/api-builder-admin
-    // dependency installed. So we guarantee that only generate config files in
-    // "development mode" and ensure immutable production environments.
-    // eslint-disable-next-line import/no-unresolved
-    require('@axway/api-builder-admin');
-    return true;
-  } catch (ex) {
-    // when we run plugin test suite @axway/api-builder-admin is not there 
-    // so we are kind of simulating production mode
-    return false;
+  /**
+   * Tests whether or not the API Builder application is in developer mode.  The test
+   * is to check to see if @axway/api-builder-admin exists.
+   *
+   * @returns {boolean} True if in developer mode.
+   */
+  isDeveloperMode: () => {
+    try {
+      // If we are in "development mode" we are going to have @axway/api-builder-admin
+      // dependency installed. So we guarantee that only generate config files in
+      // "development mode" and ensure immutable production environments.
+      // eslint-disable-next-line import/no-unresolved
+      require('@axway/api-builder-admin');
+      return true;
+    } catch (ex) {
+      // when we run plugin test suite @axway/api-builder-admin is not there 
+      // so we are kind of simulating production mode
+      return false;
+    }
   }
 }
