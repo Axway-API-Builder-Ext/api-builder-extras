2a53c33ff637e0fe67ba7957bb57d3ae1b75df08
Chris Wiechmann <cwiechmann@axway.com>
2020-11-06 08:47:22 +0100

    Update basic-config.js

--- api-builder-plugin-fn-elasticsearch/test/config/basic-config.js
+++ api-builder-plugin-fn-elasticsearch/test/config/basic-config.js
@@ -14,13 +14,7 @@ module.exports = {
 					*/
 				}, 
 				// The name to identify the client instance in the events.
-				name: 'api-builder',
-				// You can use all configuration options documented here: 
-				// https://www.elastic.co/guide/en/elasticsearch/client/javascript-api/7.x/client-configuration.html
-				ssl: {
-					ca: fs.readFileSync('C:/temp/ca.crt'),
-					rejectUnauthorized: false
-				}
+				name: 'api-builder'
 			}
 		}
 	}
