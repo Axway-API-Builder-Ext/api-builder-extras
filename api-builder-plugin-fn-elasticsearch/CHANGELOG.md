a8ed94709495bb7893b76a9ca1c46f7ef4283a98
Chris Wiechmann <cwiechmann@axway.com>
2020-12-09 15:58:41 +0100

    Making MockElasticsearchMethod available
    to external clients to run tests.

--- api-builder-plugin-fn-elasticsearch/CHANGELOG.md
+++ api-builder-plugin-fn-elasticsearch/CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [1.0.14] 2020-12-09
+### Added
+- New method: mockElasticsearchMethod to mock requests to Elasticsearch
+
 ## [1.0.13] 2020-12-09
 ### Changed
 - Test & Logging of Elasticsearch connection improved
--- api-builder-plugin-fn-elasticsearch/src/actions/ElasticsearchClient.js
+++ api-builder-plugin-fn-elasticsearch/src/actions/ElasticsearchClient.js
@@ -1,4 +1,6 @@
 const { Client } = require('@elastic/elasticsearch');
+const simple = require('simple-mock');
+const fs = require('fs');
 
 class ElasticsearchClient {
 
@@ -17,4 +19,33 @@ class ElasticsearchClient {
 	}
 }
 
-module.exports = { ElasticsearchClient }
+function mockElasticsearchMethod(client, methodName, responeFilename, shouldError) {
+    var mockedFn = simple.spy(function (params, options, callback) {
+        const resonse = JSON.parse(fs.readFileSync(responeFilename), null);
+        if(callback != undefined) {
+            if(shouldError) {
+                // Return the given response as an error
+                callback(resonse, null);
+            } else {
+                // Otherwise return the response as expected
+                callback(null, resonse);
+            }
+        } else {
+            return new Promise((resolve, reject) => {
+                if(shouldError) {
+                    reject(resonse);
+                } else {
+                    resolve(resonse);
+                }
+            });
+        }
+    });
+    // Use the extend functionality of the ES-Client to register the mocked method
+    client.extend(methodName, { force: true }, ({ makeRequest }) => {
+        return mockedFn;
+    });
+    // Return the mocked function to perform assertions
+    return mockedFn;
+}
+
+module.exports = { ElasticsearchClient, mockElasticsearchMethod }
