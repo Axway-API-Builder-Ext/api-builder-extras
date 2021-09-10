803ef299ef4cee856eb9ac8ca0223684b6d8f3f1
Chris Wiechmann <cwiechmann@axway.com>
2021-09-10 09:43:01 +0200

    Defaults the PG port to 5432 if not given

--- api-builder-plugin-dc-postgres/config/postgres.default.js
+++ api-builder-plugin-dc-postgres/config/postgres.default.js
@@ -14,7 +14,7 @@ module.exports = {
 			connectionPooling: true,
 			connectionLimit: 10,
 			host: process.env.POSTGRES_HOST,
-			port: process.env.POSTGRES_PORT,
+			port: parseInt(process.env.POSTGRES_PORT) || 5432,
 			database: process.env.POSTGRES_DB,
 			user: process.env.POSTGRES_USER,
 			password: process.env.POSTGRES_PASSWORD,
