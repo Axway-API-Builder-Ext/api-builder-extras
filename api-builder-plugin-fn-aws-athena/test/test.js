42de3a0c015f86b14b9c5988a6b9096aab2a6da9
Chris Wiechmann <cwiechmann@axway.com>
2022-02-03 09:26:26 +0100

    Refactored AWS-Athena based on updated API-Builder SDK

--- api-builder-plugin-fn-aws-athena/test/test.js
+++ api-builder-plugin-fn-aws-athena/test/test.js
@@ -1,93 +1,80 @@
 const { expect } = require('chai');
-const { MockRuntime } = require('@axway/api-builder-sdk');
+const { MockRuntime } = require('@axway/api-builder-test-utils');
+const getPlugin = require('../src');
 var simple = require('simple-mock');
 
-const getPlugin = require('../src');
-const actions = require('../src/actions');
-const validPluginConfig = require('./config/aws-athena.testconfig').pluginConfig['@axway-api-builder-ext/api-builder-plugin-fn-aws-athena'];
-const invalidPluginConfig = require('./config/aws-athena.incomplete').pluginConfig['@axway-api-builder-ext/api-builder-plugin-fn-aws-athena'];
+describe('flow-node temp-plugin', () => {
+	let plugin;
+	let flowNode;
 
-describe('Basic configuration tests for AWS Athena connector', () => {
-	let runtime;
-	before(async () => runtime = new MockRuntime(await getPlugin(validPluginConfig)));
+	const pluginConfig = require('./config/aws-athena.testconfig').pluginConfig['@axway-api-builder-ext/api-builder-plugin-fn-aws-athena'];
+
+	beforeEach(async () => {
+		plugin = await MockRuntime.loadPlugin(getPlugin, pluginConfig);
+		plugin.setOptions({
+			validateInputs: true,
+			validateOutputs: true
+		});
+		flowNode = plugin.getFlowNode('athena');
+	});
 
 	describe('#constructor', () => {
 		it('should define flow-nodes', () => {
-			expect(actions).to.be.an('object');
-			expect(actions.query).to.be.a('function');
-			expect(runtime).to.exist;
-			const flownode = runtime.getFlowNode('athena');
-			expect(flownode).to.be.a('object');
+			expect(plugin).to.be.a('object');
+			expect(plugin.getFlowNodeIds()).to.deep.equal([
+				'athena'
+			]);
+			expect(flowNode).to.be.a('object');
 
-			// Ensure the flow-node matches the spec
-			expect(flownode.name).to.equal('AWS Athena');
-			expect(flownode.description).to.equal('Flow node to query AWS Athena');
-			expect(flownode.icon).to.be.a('string');
+			expect(flowNode.name).to.equal('AWS Athena');
 		});
 
+		// It is vital to ensure that the generated node flow-nodes are valid
+		// for use in API Builder. Your unit tests should always include this
+		// validation to avoid potential issues when API Builder loads your
+		// node.
 		it('should define valid flow-nodes', () => {
-			expect(runtime.validate()).to.not.throw;
+			// if this is invalid, it will throw and fail
+			plugin.validate();
 		});
 	});
-});
 
-describe('Valid PluginConfig tests', () => {
-	let runtime;
-	before(async () => runtime = new MockRuntime(await getPlugin(validPluginConfig)));
+	describe('Valid PluginConfig tests', () => {
+		it('should error when missing required parameter: table', async () => {
+			// Disable automatic input validation (we want the action to handle this)
+			plugin.setOptions({ validateInputs: false });
 
-	describe('Query', () => {
-		it('should error when without table parameter', async () => {
-			const flowNode = runtime.getFlowNode('athena');
-			const result = await flowNode.query({
-				table: null
-			});
+			const { value, output } = await flowNode.query({ table: null });
 
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('error');
-			expect(result.args[0]).to.equal(null);
-			expect(result.args[1]).to.be.instanceOf(Error)
+			expect(value).to.be.instanceOf(Error)
 				.and.to.have.property('message', 'Missing required parameter: table');
-			expect(result.context).to.be.an('Object');
-			expect(result.context.error).instanceOf(Error)
-				.and.to.have.property('message', 'Missing required parameter: table');
+			expect(output).to.equal('error');
 		});
 
-		it('should error when without fields parameter', async () => {
-			const flowNode = runtime.getFlowNode('athena');
-			const result = await flowNode.query({
-				table: 'elb_logs'
-			});
+		it('should error when missing required parameter: fields', async () => {
+			// Disable automatic input validation (we want the action to handle this)
+			plugin.setOptions({ validateInputs: false });
 
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('error');
-			expect(result.args[0]).to.equal(null);
-			expect(result.args[1]).to.be.instanceOf(Error)
+			const { value, output } = await flowNode.query({ table: 'elb_logs', fields: null });
+
+			expect(value).to.be.instanceOf(Error)
 				.and.to.have.property('message', 'Missing required parameter: fields');
-			expect(result.context).to.be.an('Object');
-			expect(result.context.error).instanceOf(Error)
-				.and.to.have.property('message', 'Missing required parameter: fields');
+			expect(output).to.equal('error');
 		});
 
-		it('should error when without db parameter', async () => {
-			const flowNode = runtime.getFlowNode('athena');
-			const result = await flowNode.query({
-				table: 'elb_logs', fields: 'elb_name, request_ip'
-			});
+		it('should error when missing required parameter: db', async () => {
+			// Disable automatic input validation (we want the action to handle this)
+			plugin.setOptions({ validateInputs: false });
 
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('error');
-			expect(result.args[0]).to.equal(null);
-			expect(result.args[1]).to.be.instanceOf(Error)
+			const { value, output } = await flowNode.query({ table: 'elb_logs', fields: 'elb_name, request_ip', db: null });
+
+			expect(value).to.be.instanceOf(Error)
 				.and.to.have.property('message', 'Missing required parameter: db');
-			expect(result.context).to.be.an('Object');
-			expect(result.context.error).instanceOf(Error)
-				.and.to.have.property('message', 'Missing required parameter: db');
+			expect(output).to.equal('error');
 		});
 
 		it('Make a simple query without a given limit - Must be limited to default 5', async () => {
-			const flowNode = runtime.getFlowNode('athena');
-
-			simple.mock(runtime.plugin.flownodes.athena.athenaClient, 'query').callFn(function (params) {
+			var mockedClient = simple.mock(plugin.getRawPlugin().flownodes.athena.athenaClient, 'query').callFn(function (params) {
 				expect(params.sql).to.be.an('String');
 				expect(params.sql).to.equals('SELECT elb_name, request_ip FROM elb_logs LIMIT 5');
 				expect(params.db).to.be.an('String');
@@ -98,22 +85,19 @@ describe('Valid PluginConfig tests', () => {
 				]});
 			});
 
-			const result = await flowNode.query({ table: 'elb_logs', db: 'sampledb', fields: 'elb_name, request_ip'});
+			const { value, output } = await flowNode.query({ table: 'elb_logs', db: 'sampledb', fields: 'elb_name, request_ip'});
 
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('next');
-			expect(result.args).to.be.an('Array');
-			expect(result.args[1]).to.have.keys(['Items']);
-			expect(result.args[1].Items[0]).to.have.any.keys(['elb_name', 'request_ip']);
-			expect(result.args[1].Items[0]).to.not.have.any.keys(['request_port']);
-			expect(result.args[1].Items).to.have.lengthOf(2);
+			expect(mockedClient.callCount).to.equal(1);
+			expect(output).to.equal('next');
+			expect(value).to.be.an('Object');
+			expect(value).to.deep.equal({ Items: [
+				{elb_name: "elb_demo_003", request_ip: "251.165.102.100"}, 
+				{elb_name: "elb_demo_007", request_ip: "250.120.176.53"}, 
+			]});
 		});
 
 		it('Make a simple query with limit 1', async () => {
-			debugger;
-			const flowNode = runtime.getFlowNode('athena');
-
-			simple.mock(runtime.plugin.flownodes.athena.athenaClient, 'query').callFn(function (params) {
+			var mockedClient = simple.mock(plugin.getRawPlugin().flownodes.athena.athenaClient, 'query').callFn(function (params) {
 				expect(params.sql).to.be.an('String');
 				expect(params.sql).to.equals('SELECT elb_name, request_port FROM elb_logs LIMIT 1');
 				expect(params.db).to.be.an('String');
@@ -123,15 +107,14 @@ describe('Valid PluginConfig tests', () => {
 				]});
 			});
 
-			const result = await flowNode.query({ table: 'elb_logs', db: 'sampledb', fields: 'elb_name, request_port', limit: '1' });
+			const { value, output } = await flowNode.query({ table: 'elb_logs', db: 'sampledb', fields: 'elb_name, request_port', limit: '1' });
 
-			expect(result.callCount).to.equal(1);
-			expect(result.output).to.equal('next');
-			expect(result.args).to.be.an('Array');
-			expect(result.args[1]).to.have.keys(['Items']);
-			expect(result.args[1].Items[0]).to.have.any.keys(['elb_name', 'request_port']);
-			expect(result.args[1].Items[0]).to.not.have.any.keys(['request_ip']);
-			expect(result.args[1].Items).to.have.lengthOf(1);
+			expect(mockedClient.callCount).to.equal(1);
+			expect(output).to.equal('next');
+			expect(value).to.be.an('Object');
+			expect(value).to.deep.equal({ Items: [
+				{elb_name: "elb_demo_003", request_port: "24588"}
+			]});
 		}).timeout(15000);
 	});
 });
