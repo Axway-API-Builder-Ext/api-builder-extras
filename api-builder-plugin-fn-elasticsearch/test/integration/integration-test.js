815cd523ef478f03e61fb9210c99451356825d44
Chris Wiechmann <cwiechmann@axway.com>
2022-03-01 14:33:28 +0100

    Some tweaks

--- api-builder-plugin-fn-elasticsearch/test/integration/integration-test.js
+++ api-builder-plugin-fn-elasticsearch/test/integration/integration-test.js
@@ -6,8 +6,6 @@ const path = require('path');
 const simple = require('simple-mock');
 const dotenv = require('dotenv');
 
-//const { ElasticsearchClient } = require('../../../src/actions/ElasticsearchClient');
-
 describe('Integration tests', () => {
 	let plugin;
 	let flowNode;
@@ -34,6 +32,69 @@ describe('Integration tests', () => {
 		plugin = await MockRuntime.loadPlugin(getPlugin, pluginConfig, options);
 		plugin.setOptions({ validateOutputs: true });
 		flowNode = plugin.getFlowNode('elasticsearch');
+	});
+
+	describe('#Index template integration test', () => {
+		it('should follow the Not found path giving an unknown Index-Template name', async () => {
+
+			const inputParameter = { name: 'doesntexists' };
+			const { value, output } = await flowNode.getTemplate(inputParameter);
+			expect(value).to.equal('No index template found with name [doesntexists]');
+			expect(output).to.equal('notFound');
+		});
+		
+
+		it('should pass with a valid template name', async () => {
+
+			const inputParameter = { name: '.monitoring-es' };
+			const { value, output } = await flowNode.getTemplate(inputParameter);
+
+			expect(output).to.equal('next');
+			expect(value.mappings).to.exist;
+		});
+	});
+
+	describe('#Indices integration test', () => {
+
+		it('should fail if the given index-template does not exist', async () => {
+			const inputParameter = { index: "my_index_to_be_created", indexTemplate: "this_template_is_missing" };
+			const { value, output } = await flowNode.indicesCreate(inputParameter);
+
+			expect(value).to.be.instanceOf(Error)
+				.and.to.have.property('message', "The index template: 'this_template_is_missing' is missing. Index wont be created.");
+			expect(output).to.equal('error');
+		});		
+	});
+
+	describe('#Index mapping integration test', () => {
+		it('should fail giving an unknown index name', async () => {
+			const inputParameter = { index: 'doesntexists' };
+			const { value, output } = await flowNode.getMapping(inputParameter);
+
+			expect(value).to.be.instanceOf(Error)
+				.and.to.have.property('message', 'no such index [doesntexists]');
+			expect(output).to.equal('error');
+		});
+	});
+
+	describe('#Index document integration test', () => {
+		it('should fail if index does not exists and cannot be created', async () => {
+
+			const inputParameter = { index: "apigw-some-new-index", body: {message: "A document to index"}, refresh: "wait_for" };
+			const { value, output } = await flowNode.indexDocument(inputParameter);
+
+			expect(value).to.be.instanceOf(Error);
+			expect(value.message).to.contain('index_not_found_exception');
+			expect(output).to.equal('error');
+		});
+
+		it('should pass without any optional parameter', async () => {
+			const inputParameter = { index: "my-index", body: {message: "A document to index"} };
+			const { value, output } = await flowNode.indexDocument(inputParameter);
+
+			expect(value.result).to.equals("created");
+			expect(output).to.equal('next');
+		});
 	});
 
 	describe('#Search integration test', () => {
@@ -66,61 +127,6 @@ describe('Integration tests', () => {
 		});
 	});
 
-	describe('#Indices integration test', () => {
-
-		it('should fail if the given index-template does not exist', async () => {
-			const inputParameter = { index: "my_index_to_be_created", indexTemplate: "this_template_is_missing" };
-			const { value, output } = await flowNode.indicesCreate(inputParameter);
-
-			expect(value).to.be.instanceOf(Error)
-				.and.to.have.property('message', "The index template: 'this_template_is_missing' is missing. Index wont be created.");
-			expect(output).to.equal('error');
-		});		
-	});
-
-	describe('#Index template integration test', () => {
-		it('should follow the Not found path giving an unknown Index-Template name', async () => {
-
-			const inputParameter = { name: 'doesntexists' };
-			const { value, output } = await flowNode.getTemplate(inputParameter);
-			expect(value).to.equal('No index template found with name [doesntexists]');
-			expect(output).to.equal('notFound');
-		});
-		
-
-		it('should pass with a valid template name', async () => {
-
-			const inputParameter = { name: '.monitoring-es' };
-			const { value, output } = await flowNode.getTemplate(inputParameter);
-
-			expect(output).to.equal('next');
-			expect(value.mappings).to.exist;
-		});
-	});
-
-	describe('#Index mapping integration test', () => {
-		it('should fail giving an unknown index name', async () => {
-			const inputParameter = { index: 'doesntexists' };
-			const { value, output } = await flowNode.getMapping(inputParameter);
-
-			expect(value).to.be.instanceOf(Error)
-				.and.to.have.property('message', 'no such index [doesntexists]');
-			expect(output).to.equal('error');
-		});
-	});
-
-	describe('#Index document integration test', () => {
-		it('should fail if index does not exists and cannot be created', async () => {
-
-			const inputParameter = { index: "apigw-some-new-index", body: {message: "A document to index"}, refresh: "wait_for" };
-			const { value, output } = await flowNode.indexDocument(inputParameter);
-
-			expect(value).to.be.instanceOf(Error);
-			expect(value.message).to.contain('index_not_found_exception');
-			expect(output).to.equal('error');
-		});
-	});
-
 	describe('#Rollup jobs integration test', () => {
 		it('should follow notFound with an invalid Job ID', async () => {
 			const inputParameter = { id: 'unknown-job' };
@@ -129,5 +135,4 @@ describe('Integration tests', () => {
 			expect(output).to.equal('notFound');
 		});
 	});
-
 });
